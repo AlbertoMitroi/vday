41ce17e5101b2a8f5555c61000b4a6b9162db86a
AlbertoMitroi <73647184+AlbertoMitroi@users.noreply.github.com>
2025-02-05 15:52:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -61,17 +61,25 @@ function displayCat() {
 function displayCatHeart() {
     // Clear existing content in the image container
     document.getElementById('image-container').innerHTML = '';
+
     // Get the container where the image will be displayed
     var imageContainer = document.getElementById('image-container');
+
     // Create a new Image element for the cat-heart
     var catHeartImage = new Image();
-    // Set the source (file path) for the cat-heart image
     catHeartImage.src = 'cat-heart.gif'; // Assuming the cat-heart image is named "cat-heart.gif"
-    // Set alternative text for the image (for accessibility)
     catHeartImage.alt = 'Cat Heart';
-    // When the cat-heart image is fully loaded, add it to the image container
+
+    // Create the text element "Tu"
+    var textElement = document.createElement('div');
+    textElement.textContent = 'Tu';
+    textElement.classList.add('pulsating-text'); // Add CSS animation class
+
+    // When the cat-heart image is fully loaded, add elements to the image container
     catHeartImage.onload = function() {
         imageContainer.appendChild(catHeartImage);
+        imageContainer.appendChild(textElement);
+
         // Hide the options container
         document.getElementById('options').style.display = 'none';
     };
